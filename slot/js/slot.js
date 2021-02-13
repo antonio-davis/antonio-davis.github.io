3fa096e1764bc3a5882932a03ff062ea73398b4a
simone <simonepandolfi@protonmail.com>
2021-02-13 21:44:46 +0100

    scarso io

--- slot/js/slot.js
+++ slot/js/slot.js
@@ -1,7 +1,7 @@
 /**
-* Slot machine
-* Author: Saurabh Odhyan | http://odhyan.com
-*/
+ * Slot machine
+ * Author: Saurabh Odhyan | http://odhyan.com
+ */
 
 /** ******************
  *  _____ _____ _____ 
@@ -13,45 +13,45 @@
  * ******************               
  */
 
-$(document).ready(function() {
+$(document).ready(function () {
 	/**
-	* Global variables
-	*/
+	 * Global variables
+	 */
 	var completed = 0,
-		imgHeight = 1374,
+		imgHeight = 1190,
 		posArr = [
 			0, //orange
-			80, //number 7 
-			165, //bar
-			237, //guava
-			310, //banana
-			378, //cherry
-			454, //orange
-			539, //number 7
-			624, //bar
-			696, //guava
-			769, //banana
-			837, //cherry
-			913, //orange
-			1000, //number 7
-			1085, //bar
-			1157, //guava
-			1230, //banana
-			1298 //cherry
+			70, //number 7 
+			140, //bar
+			210, //guava
+			280, //banana
+			350, //cherry
+			420, //orange
+			490, //number 7
+			560, //bar
+			630, //guava
+			700, //banana
+			770, //cherry
+			840, //orange
+			910, //number 7
+			980, //bar
+			1050, //guava
+			1120, //banana
+			1190 //cherry
 		];
-	
+
 	var win = [];
-	win[0] = win[454] = win[913] = 1;
-	win[80] = win[539] = win[1000] = 2;
-	win[165] = win[624] = win[1085] = 3;
-	win[237] = win[696] = win[1157] = 4;
-	win[310] = win[769] = win[1230] = 5;
-	win[378] = win[837] = win[1298] = 6;
+	win[0] = win[420] = win[840] = 1;
+	win[70] = win[490] = win[910] = 2;
+	win[140] = win[560] = win[980] = 3;
+	win[210] = win[630] = win[1050] = 4;
+	win[280] = win[700] = win[1120] = 5;
+	win[350] = win[770] = win[1190] = 6;
 
 	/**
-	* @class Slot
-	* @constructor
-	*/
+	 * @class Slot
+	 * @constructor
+	 */
 	function Slot(el, max, step) {
 		this.speed = 0; //speed of the slot at any point of time
 		this.step = step; //speed will increase at this rate
@@ -61,22 +61,22 @@ $(document).ready(function() {
 		this.pos = null; //final position of the slot	
 
 		$(el).pan({
-            fps:30,
-            dir:'down'
-        });
+			fps: 60,
+			dir: 'down'
+		});
 		$(el).spStop();
 	}
 
 	/**
-	* @method start
-	* Starts a slot
-	*/
-	Slot.prototype.start = function() {
+	 * @method start
+	 * Starts a slot
+	 */
+	Slot.prototype.start = function () {
 		var _this = this;
 		$(_this.el).addClass('motion');
-        $(_this.el).spStart();
-		_this.si = window.setInterval(function() {
-			if(_this.speed < _this.maxSpeed) {
+		$(_this.el).spStart();
+		_this.si = window.setInterval(function () {
+			if (_this.speed < _this.maxSpeed) {
 				_this.speed += _this.step;
 				$(_this.el).spSpeed(_this.speed);
 			}
@@ -84,19 +84,19 @@ $(document).ready(function() {
 	};
 
 	/**
-	* @method stop
-	* Stops a slot
-	*/
-	Slot.prototype.stop = function() {
+	 * @method stop
+	 * Stops a slot
+	 */
+	Slot.prototype.stop = function () {
 		var _this = this,
 			limit = 30;
 		clearInterval(_this.si);
-		_this.si = window.setInterval(function() {
-			if(_this.speed > limit) {
+		_this.si = window.setInterval(function () {
+			if (_this.speed > limit) {
 				_this.speed -= _this.step;
 				$(_this.el).spSpeed(_this.speed);
 			}
-			if(_this.speed <= limit) {
+			if (_this.speed <= limit) {
 				_this.finalPos(_this.el);
 				$(_this.el).spSpeed(0);
 				$(_this.el).spStop();
@@ -108,10 +108,10 @@ $(document).ready(function() {
 	};
 
 	/**
-	* @method finalPos
-	* Finds the final position of the slot
-	*/
-	Slot.prototype.finalPos = function() {
+	 * @method finalPos
+	 * Finds the final position of the slot
+	 */
+	Slot.prototype.finalPos = function () {
 		var el = this.el,
 			el_id,
 			pos,
@@ -128,11 +128,11 @@ $(document).ready(function() {
 		pos = pos.split(' ')[1];
 		pos = parseInt(pos, 10);
 
-		for(i = 0; i < posArr.length; i++) {
-			for(j = 0;;j++) {
+		for (i = 0; i < posArr.length; i++) {
+			for (j = 0;; j++) {
 				k = posArr[i] + (imgHeight * j);
-				if(k > pos) {
-					if((k - pos) < posMin) {
+				if (k > pos) {
+					if ((k - pos) < posMin) {
 						posMin = k - pos;
 						best = k;
 						this.pos = posArr[i]; //update the final position of the slot
@@ -145,23 +145,23 @@ $(document).ready(function() {
 		best += imgHeight + 4;
 		bgPos = "0 " + best + "px";
 		$(el).animate({
-			backgroundPosition:"(" + bgPos + ")"
+			backgroundPosition: "(" + bgPos + ")"
 		}, {
 			duration: 200,
-			complete: function() {
-				completed ++;
-            }
+			complete: function () {
+				completed++;
+			}
 		});
 	};
-	
+
 	/**
-	* @method reset
-	* Reset a slot to initial state
-	*/
-    
-	Slot.prototype.reset = function() {
+	 * @method reset
+	 * Reset a slot to initial state
+	 */
+
+	Slot.prototype.reset = function () {
 		var el_id = $(this.el).attr('id');
-        $._spritely.instances[el_id].t = 0;
+		$._spritely.instances[el_id].t = 0;
 		$(this.el).css('background-position', '0px 4px');
 		this.speed = 0;
 		completed = 0;
@@ -178,7 +178,7 @@ $(document).ready(function() {
 
 	function printResult() {
 		var res;
-		if(win[a.pos] === win[b.pos] && win[a.pos] === win[c.pos]) {
+		if (win[a.pos] === win[b.pos] && win[a.pos] === win[c.pos]) {
 			res = "You Win!";
 		} else {
 			res = "You Lose";
@@ -192,48 +192,48 @@ $(document).ready(function() {
 		c = new Slot('#slot3', 70, 3);
 
 	/**
-	* Slot machine controller
-	*/
-	$('#control').click(function() {
+	 * Slot machine controller
+	 */
+	$('#control').click(function () {
 		var x;
-		if(this.innerHTML == "Start") {
+		if (this.innerHTML == "💵") {
 			a.start();
 			b.start();
 			c.start();
-			this.innerHTML = "Stop";
-			
+			this.innerHTML = "🛑";
+
 			disableControl(); //disable control until the slots reach max speed
-			
+
 			//check every 100ms if slots have reached max speed 
 			//if so, enable the control
-			x = window.setInterval(function() {
-				if(a.speed >= a.maxSpeed && b.speed >= b.maxSpeed && c.speed >= c.maxSpeed) {
+			x = window.setInterval(function () {
+				if (a.speed >= a.maxSpeed && b.speed >= b.maxSpeed && c.speed >= c.maxSpeed) {
 					enableControl();
 					window.clearInterval(x);
 				}
 			}, 100);
-		} else if(this.innerHTML == "Stop") {
+		} else if (this.innerHTML == "🛑") {
 			a.stop();
 			b.stop();
 			c.stop();
-			this.innerHTML = "Reset";
+			this.innerHTML = "🔄";
 
 			disableControl(); //disable control until the slots stop
-            
-            //check every 100ms if slots have stopped
-            //if so, enable the control
-            x = window.setInterval(function() {
-                if(a.speed === 0 && b.speed === 0 && c.speed === 0 && completed === 3) {
-                    enableControl();
-                    window.clearInterval(x);
+
+			//check every 100ms if slots have stopped
+			//if so, enable the control
+			x = window.setInterval(function () {
+				if (a.speed === 0 && b.speed === 0 && c.speed === 0 && completed === 3) {
+					enableControl();
+					window.clearInterval(x);
 					printResult();
-                }
-            }, 100);
-		} else { //reset
+				}
+			}, 100);
+		} else { // reset
 			a.reset();
 			b.reset();
 			c.reset();
-			this.innerHTML = "Start";
+			this.innerHTML = "💵";
 		}
 	});
 });
